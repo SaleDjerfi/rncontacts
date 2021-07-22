194f46f5aa8776de487951ae0d80f3a8f4bc9fd5
SaleDjerfi <aleksandardjerfi@gmail.com>
2021-07-22 14:48:08 +0200

    Login screen ui

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,35 +1,11 @@
 import React from 'react'
-import { Text } from 'react-native';
-import Container from '../../components/common/Container/Index';
-import CustomButton from '../../components/common/CustomButton';
-import Input from '../../components/common/Input';
+import LoginComponent from '../../components/Login';
 
 const Login = () => {
     const [value, onChangeText] = React.useState('');
 
     return(
-        <Container>
-            <Input
-                label='Username'
-                onChangeText={(text) => onChangeText(text)}
-                value={value}
-                iconPosition="right"
-                //error={"This field is required"}
-            />
-
-            <Input
-                label='Password'
-                onChangeText={(text) => onChangeText(text)}
-                value={value}
-                icon={<Text>HIDE</Text>}
-                iconPosition="right"
-            />
-
-            <CustomButton secondary title='Submit' loading={true} disabled={true}/>
-            <CustomButton secondary loading title='Click me'/>
-            <CustomButton primary title='Submit' loading={true} disabled={true}/>
-            <CustomButton danger title='Submit'/>
-        </Container>
+        <LoginComponent/>
     )
 }
 
